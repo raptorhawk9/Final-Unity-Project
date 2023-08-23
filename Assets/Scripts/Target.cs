fbdd249c3690f004c1a2eb1e5f09b1734fc60429
raptorhawk9 <142059090+raptorhawk9@users.noreply.github.com>
2023-08-23 10:05:03 -0700

    Target Movement commit

---
+++ Assets/Scripts/Target.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    private bool isMovingRight;
+    private float moveSpeed;
+    private float moveMin = 0.01f;
+    private float moveMax = 0.2f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartMove();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    protected void Move()
+    {
+        if (isMovingRight)
+        {
+            transform.Translate(moveSpeed, 0, 0);
+        }
+        else
+        {
+            transform.Translate(-moveSpeed, 0, 0);
+        }
+    }
+
+    private void StartMove()
+    {
+        isMovingRight = RandomBool();
+
+        moveSpeed = Random.Range(moveMin, moveMax);
+    }
+
+    private bool RandomBool()
+    {
+        return (Random.value > 0.5f);
+    }
+}
